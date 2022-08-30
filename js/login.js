4ebf9593ad6c5a42678ddcada3d1f85ba5753eb9
nachon02 <110431737+nachon02@users.noreply.github.com>
2022-08-30 11:35:37 -0300

    agrego el meta de google

--- js/login.js
+++ js/login.js
@@ -96,3 +96,11 @@ button.addEventListener("click", function validar() {
         }
     }, 1000);
 });
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+    console.log("hola"); // This is null if the 'email' scope is not present.
+}
